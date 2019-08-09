5e2de7824659ac9081e05167ff838e60f5eb49e0
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-09 23:11:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,1 @@
 # Add your variables here
-first_number = "Hello world!"
-it "contains a local variable called first_number that is assigned to a number" do
- expect(first_number).to be_an(Integer).or be_a(Float)
-end
